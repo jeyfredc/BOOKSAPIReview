73bbc4d7cbd0a5743f0772fc7d3bb940ca994952
Jeyfredc <jeyfredc@gmail.com>
2025-05-27 23:46:28 -0500

    update Program and appsettings

--- Program.cs
+++ Program.cs
@@ -6,8 +6,31 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Npgsql;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Agregar configuración de la conexión
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (string.IsNullOrEmpty(connectionString))
+{
+    // Si no está en appsettings, intenta obtenerla de la variable de entorno
+    connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    if (!string.IsNullOrEmpty(connectionString) && connectionString.StartsWith("postgres://"))
+    {
+        // Convertir de formato URL a connection string
+        var uri = new Uri(connectionString);
+        var userInfo = uri.UserInfo.Split(':');
+        connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};" +
+                          $"Username={userInfo[0]};Password={userInfo[1]};" +
+                          "SslMode=Require;Trust Server Certificate=true";
+    }
+}
+
+// Registrar la conexión
+builder.Services.AddScoped<NpgsqlConnection>(_ =>
+    new NpgsqlConnection(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -30,16 +53,11 @@ builder.Services.AddScoped<CategoryDao>();
 builder.Services.AddScoped<ISearchService, SearchService>();
 builder.Services.AddScoped<SearchDao>();
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "BooksAPIReviews", Version = "v1" });
 });
-
-// Configuración de la base de datos
-builder.Services.AddScoped<NpgsqlConnection>(sp =>
-    new NpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -56,4 +74,3 @@ app.MapControllers();
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 app.Run($"http://0.0.0.0:{port}");
-
