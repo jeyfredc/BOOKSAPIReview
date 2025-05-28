334987426d302543856bf650d7dcceb53713ada1
Jeyfredc <jeyfredc@gmail.com>
2025-05-28 08:44:26 -0500

    Modify service RegisterUser

--- Models/DTOs/UserRegisterDto.cs
+++ Models/DTOs/UserRegisterDto.cs
@@ -18,6 +18,9 @@ namespace BooksAPIReviews.Models.DTOs
 
         [Required]
         [StringLength(100)]
-        public string FullName { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
     }
 }
